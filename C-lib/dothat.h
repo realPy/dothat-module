26eb1af09040bc5c9c47afa066fb2980cf201f5e
p8mty <pete@biggs.org.uk>
2015-12-24 16:49:09 +0000

    More routines

--- C-lib/dothat.h
+++ C-lib/dothat.h
@@ -1,9 +1,10 @@
 
 void backlight_rgb(int, int, int);
 void backlight_led_rgb(int, int, int, int);
-void backlight_array(int [6][3]);
+void backlight_array(int [5][2]);
 void lcd_clear(void);
 void lcd_contrast(int);
 void lcd_cursor(int, int);
 void lcd_cursor_pos(int,int);
 void lcd_text(char *);
+void lcd_text_line(int , char *);
